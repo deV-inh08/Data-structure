b29979ddd71ca8b2d1b8486fa5ce771289bfb733
deV_inh08 <vintd2004@gmail.com>
2025-03-07 22:46:30 +0700

    implemnt method insert

--- Linked_list/index.js
+++ Linked_list/index.js
@@ -44,11 +44,41 @@ class LinkedList {
         return array
     }
 
+    // insert with position
+    insert(index, value) {
+        if(index >= this.length) {
+            return this.append(value)
+        }
+        if(index === 0) {
+            return this.prepend(value)
+        }
+        const newNode = new Node(value)
+
+        const leader = this.findNodeBefore(index - 1)
+        let holdingPointer = leader.next
+        leader.next = newNode
+        newNode.next = holdingPointer
+        return this.printList()
+    }
+
+    findNodeBefore(index) {
+        let count = 0
+        let currentNode = this.head
+        while(index !== count) {
+            currentNode = currentNode.next
+            count++
+        }
+        return currentNode
+    }
+       
+
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.prepend(1)
 myLinkedList.append(16)
+myLinkedList.insert(2, 99)
+myLinkedList.insert(3, 40)
 
 console.log(myLinkedList.printList())
