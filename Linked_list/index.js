3ea30d5decb76058a4edea056beba017b656fec9
deV_inh08 <vintd2004@gmail.com>
2025-03-07 21:57:07 +0700

    implement printList

--- Linked_list/index.js
+++ Linked_list/index.js
@@ -1,3 +1,10 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
 class LinkedList {
     constructor(value) {
        this.head = {
@@ -10,17 +17,38 @@ class LinkedList {
 
     // method append
     append(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value)
         this.tail.next = newNode
         this.tail = newNode
         this.length++
         return this
     }
+
+    // prepend method -> add newNode in first Linkedlist
+    prepend(value) {
+        const newNode = new Node(value)
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+    }
+
+    // printList
+    printList() {
+        const array = [];
+        let currentNode= this.head;
+        while(currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
-console.log(myLinkedList)
+myLinkedList.prepend(1)
+myLinkedList.append(16)
+
+console.log(myLinkedList.printList())
